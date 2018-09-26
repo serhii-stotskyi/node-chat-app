4f93a066aab1686865540302e440eefa8248e709
Serhii Stotskyi <itp.stotsky@yandex.ua>
2018-09-26 22:44:45 +0300

    add form for displaying messages

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,11 +17,7 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        socket.broadcast.emit('newMessage', {
-                from: message.from,
-                text: message.text,
-                createdAt: new Date().getTime()
-            });
+        io.emit('newMessage', generateMessage(message.from, message.text) );
     });
 
     socket.on('disconnect', () => {
---
+++ server/utils/message.test.js
@@ -0,0 +1,15 @@
+let expect = require('expect');
+
+let {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () =>{
+        let from = 'me';
+        let text = 'Some message';
+        let message = generateMessage(from, text);
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, text})
+
+        generateMessage()
+    })
+});
